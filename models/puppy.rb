a68912889b7ee79ae91f81029c7a172298698ad9
chic-page-6502 <zacharytq@gmail.com>
2020-08-31 16:17:48 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -3,8 +3,8 @@ class Puppy
   attr_accessor :name, :breed, :age
 
   def initialize(name, breed, age)
-    @name = :name
-    @breed = :breed
-    @age = :age
+    @name = name
+    @breed = breed
+    @age = age
   end
 end
